2b245832062e60d0f5f0dfb7c77b160358939fb4
SierraRhodes <alreadywrote@gmail.com>
2023-03-24 18:33:27 -0700

    Add loop to parse through data from api

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './CurrencyService.js';
+
+CurrencyService.getCurrency()
+  .then((data) => {
+    const rates = data.conversion_rates;
+    const dropdown = document.getElementBy("currency-dropdown");
+
+    for(const currency in rates) {
+      const option = document.createElement("option");
+      option.value = currency;
+      option.text = currency + "(" + rates[currency] + ")";
+      dropdown.add(option);
+    }
+
+
+  });
